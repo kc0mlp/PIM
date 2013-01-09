91928a375886b1188871fce7a0b562ede82411a8
Kael Hankins <kc0mlp@KC-De.(none)>
2013-01-08 19:36:20 -0600

    Initial Commit, following rails project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PIM::Application.config.secret_token = '5cc12c922163460977658e027d70af68bf63c1581fe1f0c3f87470e75568f0e92ffc692111586e63d1820b10b650f95780716e3a75b783b18db5479f9ecf7a84'

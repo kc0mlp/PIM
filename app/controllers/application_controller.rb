ba56f869b9a26786296e1a2c16eef5724663e118
Kael Hankins <kc0mlp@KC-De.(none)>
2013-01-12 15:41:48 -0600

    Adjusted posts#index to a more blog like format, added display for create and update times. Added zimezone set functionality to config. (To be replaced with user data after auth system is in place) Removed show, edit and destroy options from posts#index as these will be back end eventually.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  around_filter :set_time_zone
+  
+  def set_time_zone
+    old_time_zone = Time.zone
+    Time.zone = APP_CONFIG.config['timezone']  #Needs adjusted for login eventually.
+    #Something like:  Time.zone = current_user.time_zone if logged_in?
+    yield
+  ensure
+    Time.zone = old_time_zone
+  end
 end
